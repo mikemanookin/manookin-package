58c1f47d71438334a8afec733045e9036fcb1e9f
Michael Manookin <michaelmanookin@D-10-19-3-0.dhcp4.washington.edu>
2019-06-25 09:16:33 -0700

    Added TemporalNoise protocol to replace GaussianNoise.

---
+++ StimulusPackages/general-package/+manookinlab/+protocols/TemporalNoise.m
@@ -0,0 +1,161 @@
+classdef TemporalNoise < manookinlab.protocols.ManookinLabStageProtocol
+    
+    properties
+        amp                             % Output amplifier
+        preTime = 250                   % Stim leading duration (ms)
+        stimTime = 10000                % Stim duration (ms)
+        tailTime = 250                  % Stim trailing duration (ms)
+        randsPerRep = 7                 % Number of random seeds per repeat
+        noiseContrast = 1/3             % Noise contrast (0-1)
+        radius = 100                    % Spot/annulus inner radius in microns.
+        backgroundIntensity = 0.5       % Background light intensity (0-1) 
+        noiseClass = 'gaussian'         % Noise type (binary or Gaussian)
+        stimulusClass = 'spot'          % Stimulus class
+        chromaticClass = 'achromatic'   % Chromatic class
+        onlineAnalysis = 'extracellular'% Online analysis type.
+        numberOfAverages = uint16(50)   % Number of epochs
+    end
+    
+    properties (Hidden)
+        ampType
+        noiseClassType = symphonyui.core.PropertyType('char', 'row', {'binary','gaussian','uniform'})
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'spikes_CClamp', 'subthresh_CClamp', 'analog'})
+        stimulusClassType = symphonyui.core.PropertyType('char', 'row', {'spot','annulus', 'full-field'})
+        seed
+        noiseStream
+        frameSeq
+        radiusPix
+    end
+    
+    methods
+        
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+        
+        function prepareRun(obj)
+            prepareRun@manookinlab.protocols.ManookinLabStageProtocol(obj);
+            
+            obj.showFigure('symphonyui.builtin.figures.ResponseFigure', obj.rig.getDevice(obj.amp));
+            
+            if ~strcmp(obj.onlineAnalysis, 'none')
+                obj.showFigure('manookinlab.figures.TemporalNoiseFigure', ...
+                    obj.rig.getDevice(obj.amp),'recordingType', obj.onlineAnalysis, 'noiseClass', obj.noiseClass,...
+                    'preTime', obj.preTime, 'stimTime', obj.stimTime, ...
+                    'frameRate', obj.frameRate, 'numFrames', floor(obj.stimTime/1000 * obj.frameRate), 'frameDwell', 1, ...
+                    'stdev', obj.noiseContrast*0.3, 'frequencyCutoff', 0, 'numberOfFilters', 0, ...
+                    'correlation', 0, 'stimulusClass', 'Stage');
+                
+                obj.showFigure('manookinlab.figures.MeanResponseFigure', ...
+                    obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis,...
+                    'sweepColor',zeros(1,3),...
+                    'groupBy',{'backgroundClass'});
+            end
+            
+            obj.radiusPix = obj.rig.getDevice('Stage').um2pix(obj.radius);
+        end
+        
+        function p = createPresentation(obj)
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+            p.setBackgroundColor(obj.backgroundIntensity);
+            
+            if strcmp(obj.stimulusClass, 'spot')
+                spot = stage.builtin.stimuli.Ellipse();
+                spot.radiusX = obj.radius;
+                spot.radiusY = obj.radius; 
+                spot.position = obj.canvasSize/2;
+            else
+                spot = stage.builtin.stimuli.Rectangle();
+                spot.size = obj.canvasSize;
+                spot.position = obj.canvasSize/2;
+                spot.orientation = 0;
+            end
+            spot.color = obj.backgroundIntensity;
+            
+            % Add the stimulus to the presentation.
+            p.addStimulus(spot);
+            
+            % Add a center mask if it's an annulus.
+            if strcmp(obj.stimulusClass, 'annulus')
+                mask = stage.builtin.stimuli.Ellipse();
+                mask.radiusX = obj.radius;
+                mask.radiusY = obj.radius;
+                mask.position = obj.canvasSize/2;
+                mask.color = obj.backgroundIntensity; 
+                p.addStimulus(mask);
+            end
+            
+            % Control when the spot is visible.
+            spotVisible = stage.builtin.controllers.PropertyController(spot, 'visible', ...
+                @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+            p.addController(spotVisible);
+            
+            % Control the spot color.
+            if strcmpi(obj.noiseClass, 'gaussian')
+                colorController = stage.builtin.controllers.PropertyController(spot, 'color', ...
+                    @(state)getSpotAchromaticGaussian(obj, state.time - obj.preTime * 1e-3));
+            elseif strcmpi(obj.noiseClass, 'binary')
+                colorController = stage.builtin.controllers.PropertyController(spot, 'color', ...
+                    @(state)getSpotAchromaticBinary(obj, state.time - obj.preTime * 1e-3));
+            else
+                colorController = stage.builtin.controllers.PropertyController(spot, 'color', ...
+                    @(state)getSpotAchromaticUniform(obj, state.time - obj.preTime * 1e-3));
+            end
+            p.addController(colorController);
+            
+            function c = getSpotAchromaticGaussian(obj, time)
+                if time > 0 && time <= obj.stimTime*1e-3
+                    c = obj.noiseContrast * 0.3 * obj.noiseStream.randn * obj.backgroundIntensity + obj.backgroundIntensity;
+                else
+                    c = obj.backgroundIntensity;
+                end
+            end
+            
+            function c = getSpotAchromaticBinary(obj, time)
+                if time > 0 && time <= obj.stimTime*1e-3
+                    c = obj.noiseContrast * (2*(obj.noiseStream.rand>0.5)-1) * obj.backgroundIntensity + obj.backgroundIntensity;
+                else
+                    c = obj.backgroundIntensity;
+                end
+            end
+            
+            function c = getSpotAchromaticUniform(obj, time)
+                if time > 0 && time <= obj.stimTime*1e-3
+                    c = obj.noiseContrast * (2*obj.noiseStream.rand-1) * obj.backgroundIntensity + obj.backgroundIntensity;
+                else
+                    c = obj.backgroundIntensity;
+                end
+            end
+        end
+  
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@manookinlab.protocols.ManookinLabStageProtocol(obj, epoch);
+            
+            % Deal with the seed.
+            if obj.randsPerRep <= 0 
+                obj.seed = 1;
+            elseif obj.randsPerRep > 0 && (mod(obj.numEpochsCompleted+1,obj.randsPerRep+1) == 0)
+                obj.seed = 1;
+            else
+                obj.seed = RandStream.shuffleSeed;
+            end
+            
+            % Seed the random number generators.
+            obj.noiseStream = RandStream('mt19937ar', 'Seed', obj.seed);
+            
+            % Save the seed.
+            epoch.addParameter('seed', obj.seed);
+        end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+    end
+    
+end
