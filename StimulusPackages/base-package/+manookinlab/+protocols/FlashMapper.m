11a2ae32ef5c3a0b67644c98a464929686963e37
mikemanookin <mikemanookin76@gmail.com>
2021-02-08 14:28:44 -0800

    Initial commit of FlashMapper protocol.

---
+++ StimulusPackages/base-package/+manookinlab/+protocols/FlashMapper.m
@@ -0,0 +1,100 @@
+classdef FlashMapper < manookinlab.protocols.ManookinLabStageProtocol
+    properties
+        amp                             % Output amplifier
+        preTime = 250                   % Stimulus leading duration (ms)
+        stimTime = 500                  % Stimulus duration (ms)
+        tailTime = 250                  % Stimulus trailing duration (ms)
+        gridWidth = 250                 % Width of mapping grid (microns)
+        stixelSize = 50                 % Stixel edge size (microns)
+        contrast = 1.0                  % Contrast (0 - 1)
+        backgroundIntensity = 0.5       % Background light intensity (0-1)
+        onlineAnalysis = 'extracellular' % Online analysis type.
+        numberOfAverages = uint16(210)  % Number of epochs
+    end
+    
+    properties (Hidden)
+        ampType
+        onlineAnalysisType = symphonyui.core.PropertyType('char', 'row', {'none', 'extracellular', 'spikes_CClamp', 'subthresh_CClamp', 'analog'})
+        stixelSizePix
+        gridWidthPix
+        intensity
+        stimContrast
+        positions
+        position
+        numChecks
+    end
+    
+    methods
+        function didSetRig(obj)
+            didSetRig@edu.washington.riekelab.protocols.RiekeLabStageProtocol(obj);
+            
+            [obj.amp, obj.ampType] = obj.createDeviceNamesProperty('Amp');
+        end
+        
+        function prepareRun(obj)
+            prepareRun@manookinlab.protocols.ManookinLabStageProtocol(obj);
+            
+            obj.stixelSizePix = obj.rig.getDevice('Stage').um2pix(obj.stixelSize);
+            obj.gridWidthPix = obj.rig.getDevice('Stage').um2pix(obj.gridWidth);
+
+            % Get the number of checkers
+            edgeChecks = ceil(obj.gridWidthPix / obj.stixelSizePix);
+            obj.numChecks = edgeChecks^2;
+            [x,y] = meshgrid(linspace(-obj.stixelSizePix*edgeChecks/2+obj.stixelSizePix/2,obj.stixelSizePix*edgeChecks/2-obj.stixelSizePix/2,edgeChecks));
+            obj.positions = [x(:), y(:)];
+            
+            
+            if ~strcmp(obj.onlineAnalysis, 'none')
+                obj.showFigure('manookinlab.figures.MeanResponseFigure', ...
+                    obj.rig.getDevice(obj.amp),'recordingType',obj.onlineAnalysis,...
+                    'sweepColor',[0,0,0],...
+                    'groupBy',{'frameRate'});
+            end
+        end
+        
+        function p = createPresentation(obj)
+
+            p = stage.core.Presentation((obj.preTime + obj.stimTime + obj.tailTime) * 1e-3);
+            p.setBackgroundColor(obj.backgroundIntensity);
+            
+            rect = stage.builtin.stimuli.Rectangle();
+            rect.size = obj.stixelSizePix*ones(1,2);
+            rect.position = obj.canvasSize/2 + obj.position;
+            rect.orientation = 0;
+            rect.color = obj.intensity;
+            
+            % Add the stimulus to the presentation.
+            p.addStimulus(rect);
+            
+            barVisible = stage.builtin.controllers.PropertyController(rect, 'visible', ...
+                @(state)state.time >= obj.preTime * 1e-3 && state.time < (obj.preTime + obj.stimTime) * 1e-3);
+            p.addController(barVisible);
+        end
+        
+        function prepareEpoch(obj, epoch)
+            prepareEpoch@manookinlab.protocols.ManookinLabStageProtocol(obj, epoch);
+            
+            if mod(obj.numEpochsCompleted,2) == 0
+                obj.stimContrast = obj.contrast;
+            else
+                obj.stimContrast = -obj.contrast;
+            end
+            obj.intensity = obj.stimContrast*obj.backgroundIntensity + obj.backgroundIntensity;
+            
+            obj.position = obj.positions(mod(floor(obj.numEpochsCompleted/2),length(obj.positions))+1);
+            
+            epoch.addParameter('numChecks',obj.numChecks);
+            epoch.addParameter('position', obj.position);
+            epoch.addParameter('stimContrast', obj.stimContrast);
+        end
+        
+        function tf = shouldContinuePreparingEpochs(obj)
+            tf = obj.numEpochsPrepared < obj.numberOfAverages;
+        end
+        
+        function tf = shouldContinueRun(obj)
+            tf = obj.numEpochsCompleted < obj.numberOfAverages;
+        end
+    end
+    
+end
