6423d4abca9ba17854b533dcce83787f3c5b70b5
Mike Manookin <manookin@uw.edu>
2023-03-23 09:52:24 -0700

    Update RandomWalkColor.m

--- StimulusPackages/celltyping-package/+manookinlab/+protocols/RandomWalkColor.m
+++ StimulusPackages/celltyping-package/+manookinlab/+protocols/RandomWalkColor.m
@@ -205,7 +205,6 @@ classdef RandomWalkColor < manookinlab.protocols.ManookinLabStageProtocol
             end
             
             if strcmp(obj.correlationClass, 'OU')
-%                 obj.spotPositions = obj.getOUTrajectory(obj.stimTime*1e-3+2, 'motionSpeed', obj.stimulusSpeed);
                 obj.spotPositions = manookinlab.util.getOUTrajectory2d(obj.stimTime*1e-3+2, obj.seed, 'motionSpeed', obj.stimulusSpeed);
             else
                 obj.spotPositions = manookinlab.util.getHMMTrajectory2d(obj.stimTime*1e-3+2, obj.seed, 'motionSpeed', obj.stimulusSpeed);
@@ -217,57 +216,6 @@ classdef RandomWalkColor < manookinlab.protocols.ManookinLabStageProtocol
             epoch.addParameter('contrast',obj.contrast);
             epoch.addParameter('seed', obj.seed);
             epoch.addParameter('backgroundCondition',obj.backgroundCondition)
-            %epoch.addParameter('spotX',obj.spotPositions(:,1)');
-            %epoch.addParameter('spotY',obj.spotPositions(:,2)');
-        end
-        
-        function positions = getOUTrajectory(obj, duration, seed, varargin)
-            ip = inputParser();
-            ip.addParameter('correlationTime', 20, @(x)isfloat(x));
-            ip.addParameter('frameRate', 60.0, @(x)isfloat(x));
-            ip.addParameter('motionSpeed', 700.0, @(x)isfloat(x)); % Motion speed in pixels / sec
-
-            % Parse the inputs.
-            ip.parse(varargin{:});
-
-            % Get the field names from the input parser.
-            fnames = fieldnames(ip.Results);
-
-            % Create the parameters structure.
-            params = struct();
-            for jj = 1 : length(fnames)
-                params.(fnames{jj}) = ip.Results.(fnames{jj});
-            end
-            
-            dt = 1 / params.frameRate;
-            T = 0 : dt : (duration+40)-dt;
-            positionStream = RandStream('mt19937ar', 'Seed', seed);
-
-            D_OU = 2.7e6; %dynamical range
-            omega = params.correlationTime/2.12;   % omega = G/(2w)=1.06; follow Bielak's overdamped dynamics/ 2015PNAS
-
-            % Get your position vector (x,y).
-            positions = zeros(length(T), 2);
-            % Velocity vectors (x,y)
-            V = zeros(length(T), 2);
-            v_noise = positionStream.randn(length(T), 2);
-
-            % Update the velocities and positions on each time step according to the
-            % OU algorithm.
-            for t = 1 : length(T)-1
-                positions(t+1,:) = (1-dt*params.correlationTime/(2.12)^2)*positions(t,:)+sqrt(dt*D_OU) * v_noise(t,:);
-            end
-
-            speed = sqrt(sum(diff(positions).^2,2));
-            % Get a smoothed estimate of the speed.
-            % speed = sqrt(sum(diff(movmean(positions,15)).^2,2));
-            avgSpeed = mean(speed)*params.frameRate;
-
-            % Adjust the values to the indicated speed.
-            positions = params.motionSpeed*positions/avgSpeed;
-
-            % Get the frames for presentation from the end.
-            positions = positions(end-(ceil(duration*params.frameRate))+1:end,:);
         end
         
         function stimTime = get.stimTime(obj)
