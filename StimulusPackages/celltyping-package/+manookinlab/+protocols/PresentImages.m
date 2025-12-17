36e70e4e5cff98c0606ed8e90b9bee66920512f9
Vyom Raval <vyomr@uw.edu>
2025-12-17 13:55:51 -0800

    Update PresentImages.m

--- StimulusPackages/celltyping-package/+manookinlab/+protocols/PresentImages.m
+++ StimulusPackages/celltyping-package/+manookinlab/+protocols/PresentImages.m
@@ -60,6 +60,7 @@ classdef PresentImages < manookinlab.protocols.ManookinLabStageProtocol
         imagesPerDir
         fullImagePaths
         validImageExtensions = {'.png','.jpg','.jpeg','.tif','.tiff'}
+        expectedRefreshRate
         preFrames
         tailFrames
         stimFrames
@@ -88,11 +89,11 @@ classdef PresentImages < manookinlab.protocols.ManookinLabStageProtocol
             obj.outerMaskRadiusPix = obj.rig.getDevice('Stage').um2pix(obj.outerMaskDiameter)/2.0;
             
             % Calcualate the number of flash and gap frames.
-            expectedRefreshRate = obj.rig.getDevice('Stage').getExpectedRefreshRate();
-            obj.preFrames = round((obj.preTime * 1e-3) * expectedRefreshRate);
-            obj.flashFrames = round((obj.flashTime * 1e-3) * expectedRefreshRate);
-            obj.gapFrames = round((obj.gapTime * 1e-3) * expectedRefreshRate);
-            obj.tailFrames = round((obj.tailTime * 1e-3) * expectedRefreshRate);
+            obj.expectedRefreshRate = obj.rig.getDevice('Stage').getExpectedRefreshRate();
+            obj.preFrames = round((obj.preTime * 1e-3) * obj.expectedRefreshRate);
+            obj.flashFrames = round((obj.flashTime * 1e-3) * obj.expectedRefreshRate);
+            obj.gapFrames = round((obj.gapTime * 1e-3) * obj.expectedRefreshRate);
+            obj.tailFrames = round((obj.tailTime * 1e-3) * obj.expectedRefreshRate);
             obj.stimFrames = round((obj.flashFrames + obj.gapFrames) * obj.imagesPerEpoch);
             
             % General directory
